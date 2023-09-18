4e31e74f69217b3b1f977bdbb58ff78154d6a5e3
“Slastenov <vtb4055018@vtb.ru>
2023-09-18 08:22:03 +0300

    add parametrized class Lion

---
+++ src/test/java/com/example/LionParametrizedTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class LionParametrizedTest {
+
+    private final String sex;
+    private final boolean expectedResult;
+
+    public LionParametrizedTest(String sex, boolean expectedResult) {
+        this.sex = sex;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion (sex);
+        Assert.assertEquals(expectedResult, lion.doesHaveMane());
+    }
+}
